279cd3db3cd6152a0b84cff864027507d5b95d55
ira <Irina@git.ira.ru>
2020-04-23 20:53:13 +0300

    change 6.1, add ignore list

--- home_work_06/6.1.py
+++ home_work_06/6.1.py
@@ -10,10 +10,10 @@ class TrafficLight:
         pos = 1  # Индекс цветов из списка
         p = 1  # Для определения границ, меняет свой знак в строке 18
         for i in range(10):
-            # print(self.__color[pos])  # Вывод по очереди в новой строке
+            print(f"\r{self.__color[pos]}", end="")  # Вывод по очереди в новой строке
             # stderr.write(f"{self.__color[pos]:7}\r")  # Выводит только в консоли
             # sleep(0.5)  # С задержкой
-            stdout.write('\r' + self.__color[pos])  # Вывод в одной строке
+            # stdout.write('\r' + self.__color[pos])  # Вывод в одной строке
             sleep(2) if self.__color[pos] == 'yellow' else sleep(7)
             p = -p if pos >= (len(self.__color) - 1) or pos <= 0 else p
             pos += p
